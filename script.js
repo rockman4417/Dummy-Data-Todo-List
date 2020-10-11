6bd994a54b4b8f229134306aabcaeab9b2a438c2
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2020-10-10 19:21:32 -0500

    added the rest of the features

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ let arrayOfTodos = [
   },
 ];
 
-let x = 20;
+const list = document.getElementById("todo-list");
 let node;
 let button;
-let userID;
-
+let x = 20;
+let selectedId;
+let filteredTodos = [];
 let complete = document.querySelectorAll(".todo-button");
+let buttons = [];
+let textnode;
+let completedList = [];
+let incompletedList = [];
+let copiedArray = [];
 
 const fetchTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
@@ -31,19 +37,25 @@ const logTodos = () => {
 };
 
 const populateTodos = () => {
-  let buttons = [];
-  for (let i = 0; i < x; i++) {
+  for (let i = 0 + x - 20; i < x; i++) {
     node = document.createElement("li");
-    document.getElementById("todo-list").appendChild(node);
+    list.appendChild(node);
     node.classList.add(i);
     button = document.createElement("button");
+    button.innerHTML = "done";
     button.classList.add("todo-button");
-    let textnode = document.createTextNode(arrayOfTodos[i].title);
+    textnode = document.createTextNode(arrayOfTodos[i].title);
     node.appendChild(textnode);
     buttons.push(button);
     buttons[i].onclick = () => {
+      console.log(buttons[i].parentElement);
       buttons[i].parentElement.classList.add("completed");
       buttons[i].parentElement.classList.remove("incomplete");
+      buttons[i].classList.add("hide");
+      completedList.push(incompletedList[i]);
+      incompletedList = incompletedList.filter(
+        (array) => array != incompletedList[i]
+      );
     };
 
     if (arrayOfTodos[i].completed == false) {
@@ -54,8 +66,97 @@ const populateTodos = () => {
       node.classList.add("completed");
     }
   }
-  userID = document.getElementById("myInput").value;
-  console.log(userID);
 
-  x + 20;
+  x += 20;
+};
+
+const filterTodos = () => {
+  buttons = [];
+  list.innerHTML = "";
+  selectedId = document.getElementById("myInput").value;
+
+  filteredTodos = arrayOfTodos.filter((array) => array.userId == selectedId);
+  completedList = filteredTodos.filter((array) => array.completed);
+  incompletedList = filteredTodos.filter((array) => array.completed == false);
+  // console.log(filteredTodos);
+  // console.log(completedList);
+  // console.log(incompletedList);
+
+  for (let i = 0; i < filteredTodos.length; i++) {
+    node = document.createElement("li");
+    list.appendChild(node);
+    node.classList.add(i);
+    button = document.createElement("button");
+    button.innerHTML = "done";
+    button.classList.add("todo-button");
+    textnode = document.createTextNode(filteredTodos[i].title);
+    node.appendChild(textnode);
+    buttons.push(button);
+    buttons[i].onclick = () => {
+      buttons[i].parentElement.classList.add("completed");
+      buttons[i].parentElement.classList.remove("incomplete");
+      buttons[i].classList.add("hide");
+      completedList.push(incompletedList[i]);
+      incompletedList = incompletedList.filter(
+        (array) => array != incompletedList[i]
+      );
+    };
+
+    if (arrayOfTodos[i].completed == false) {
+      node.classList.add("incomplete");
+
+      node.appendChild(button);
+    } else {
+      node.classList.add("completed");
+    }
+  }
+};
+
+const showComplete = () => {
+  buttons = [];
+  list.innerHTML = "";
+
+  for (let i = 0; i < completedList.length - 1; i++) {
+    node = document.createElement("li");
+    list.appendChild(node);
+    node.classList.add(i);
+
+    textnode = document.createTextNode(completedList[i].title);
+    node.appendChild(textnode);
+
+    node.classList.add("completed");
+  }
+};
+
+const showIncomplete = () => {
+  buttons = [];
+  list.innerHTML = "";
+
+  for (let i = 0; i < incompletedList.length; i++) {
+    node = document.createElement("li");
+    list.appendChild(node);
+    node.classList.add(i);
+    button = document.createElement("button");
+    button.innerHTML = "done";
+    button.classList.add("todo-button");
+
+    textnode = document.createTextNode(incompletedList[i].title);
+    node.appendChild(textnode);
+    buttons.push(button);
+    buttons[i].onclick = () => {
+      buttons[i].parentElement.classList.add("completed");
+      buttons[i].parentElement.classList.remove("incomplete");
+      buttons[i].classList.add("hide");
+      console.log(i);
+      completedList.push(incompletedList[i]);
+
+      incompletedList = incompletedList.filter(
+        (listItem) => listItem !== incompletedList[i]
+      );
+    };
+
+    node.classList.add("incomplete");
+
+    node.appendChild(button);
+  }
 };
